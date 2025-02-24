560b273edce0e18d5010a715d1e3a4a50bb0ae1a
SaikatDeb12 <saikat.rcld@gmail.com>
2025-02-24 17:38:58 +0530

    basic front UI

--- src/Component/Home/Home.jsx
+++ src/Component/Home/Home.jsx
@@ -5,11 +5,13 @@ const Home = () => {
     <div className={styles.container}>
       <div className={styles.header}>
         <div className={styles.left}>
-          <p className={styles.heading}></p>
+          <p className={styles.heading}>Project Fair</p>
           <p className={styles.desc}>Lorem ipsum dolor sit amet consectetur.</p>
           <button>Get started</button>
         </div>
-        <div className={styles.right}></div>
+        <div className={styles.right}>
+          <img src="/designer.svg" alt="Project" />
+        </div>
       </div>
     </div>
   );
